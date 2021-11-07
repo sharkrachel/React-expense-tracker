8290b96d58c9242993f8c1574f052109169256ab
Rachel Sipes <rasipes@gmail.com>
2021-11-07 11:21:33 -0500

    updated folder structure

--- react-expense-tracker/src/components/Expenses/ExpenseItem.js
+++ react-expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,6 @@
+import Expenses from './Expenses';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
--- react-expense-tracker/src/components/Expenses/Expenses.js
+++ react-expense-tracker/src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import ExpenseItem from "../Expenses/ExpenseItem";
+import Card from "../UI/Card";
 import './Expenses.css';
 
 function Expenses(props) {
