81fe77da29e71d43f9ef1e9b543df7aff5ef4bd4
Rachel Sipes <rasipes@gmail.com>
2021-11-07 09:20:38 -0500

    added expense item component, js and css files

--- react-expense-tracker/src/components/ExpenseItem.js
+++ react-expense-tracker/src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        <div className="expense-item">
+            <div>November 28, 2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+            </div>
+            <div className="expense-item__price">$338.83</div>
+        </div>);
 }
 
 export default ExpenseItem;
