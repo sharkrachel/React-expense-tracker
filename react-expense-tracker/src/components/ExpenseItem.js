70340b304e4ef22b9e98e5e4e30fdf38477d2ed3
Rachel Sipes <rasipes@gmail.com>
2021-11-07 09:30:15 -0500

    updated hard text with variables

--- react-expense-tracker/src/components/ExpenseItem.js
+++ react-expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 10, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 338.83;
+
     return (
         <div className="expense-item">
-            <div>November 28, 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
+                <h2>{expenseTitle}</h2>
             </div>
-            <div className="expense-item__price">$338.83</div>
+            <div className="expense-item__price">${expenseAmount}</div>
         </div>);
 }
 
