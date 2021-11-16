e301e10ce6bf7ed82bd033584dafa515a9f29cab
Rachel Sipes <rasipes@gmail.com>
2021-11-16 12:14:12 -0500

    added useState to ExpenseItem.js

--- react-expense-tracker/src/components/Expenses/ExpenseItem.js
+++ react-expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,14 @@
-import Expenses from './Expenses';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    let title = props.title;
+   const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
+        setTitle('Updated!');
         console.log('Clicked!!!')
     } 
 
@@ -15,7 +16,7 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{itle}</h2>
+                <h2>{title}</h2>
             </div>
             <div className="expense-item__price">${props.amount}</div>
             <button onClick={clickHandler}>Change Title</button>
