9913a214b9f526f4518ce4b67cb72d785a62817a
Rachel Sipes <rasipes@gmail.com>
2021-11-16 12:37:44 -0500

    added form inputs

--- react-expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ react-expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm  = () => {
- return <form></form>
+ return <form>
+     <div className="new-expense__controls">
+         <div className="new-expense__control">
+             <label>Title</label>
+             <input type="text" />
+         </div>
+         <div className="new-expense__control">
+             <label>Amount</label>
+             <input type="number" min="0.01" ste-="0.01" />
+         </div>
+         <div className="new-expense__control">
+             <label>Date</label>
+             <input type="date" min="2020-01-01" max="2023-12-31" />
+         </div>
+     </div>
+ </form>
 
 };
 
