2e9573348f240e9040aee936db1ebeaf0663ac25
Rachel Sipes <rasipes@gmail.com>
2021-11-16 14:04:26 -0500

    added submit handler and prevent default method

--- react-expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ react-expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -3,40 +3,29 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '', 
-        enteredAmount: '', 
-        enteredDate: ''
-    });
 
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput, 
-            enteredTitle: event.target.value
-        })
+        setEnteredTitle(event.target.value);
     };
 
     const amountChangeHandler = (event) => {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput, 
-            enteredAmount: event.target.value
-        })
-    };
-
-    const datechangeHandler = (event) => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput, 
-            enteredDate: event.target.value
-        })
+        setEnteredAmount(event.target.value);
     }
 
-    return <form>
+    const datechangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
@@ -52,7 +41,7 @@ const ExpenseForm = () => {
             </div>
         </div>
         <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+            <button type="submit" >Add Expense</button>
         </div>
     </form>
 
